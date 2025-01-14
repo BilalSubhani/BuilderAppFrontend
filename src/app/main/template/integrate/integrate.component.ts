e680960d8a4bb7a72e6f7fb49ca6c755d3dd7acd
BilalSubhani <mailatbilalsubhani@gmail.com>
2025-01-14 17:11:44 +0500

    Template Integrate is also editable now

--- src/app/main/template/integrate/integrate.component.ts
+++ src/app/main/template/integrate/integrate.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 import { MainService } from '../../main.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-integrate',
   templateUrl: './integrate.component.html',
   styleUrls: ['./integrate.component.less'],
-  imports:[CommonModule]
+  imports:[CommonModule, FormsModule]
 })
 export class IntegrateTemplateComponent {
 
@@ -53,4 +54,48 @@ export class IntegrateTemplateComponent {
   ngOnDestroy() {
     this.subscription?.unsubscribe();
   }
+
+  // Editable
+  // ----------------------------------------------------------------------------
+
+  editingField: string | null = null;
+  originalValue: string | null = null;
+  updatedValue: string | null = null;
+
+  startEditing(field: 'smallHeading' | 'title' | 'body' | 'button') {
+    this.editingField = field;
+    this.originalValue = this.integrateData[field];
+    this.updatedValue = this.originalValue;
+  }
+
+  saveField() {
+    if (this.editingField) {
+      this.integrateData[this.editingField] = this.updatedValue!;
+      console.log('Updated Data:', this.integrateData);
+    }
+    this.resetEditing();
+  }
+
+  cancelEditing() {
+    if (this.editingField) {
+      this.integrateData[this.editingField] = this.originalValue!;
+    }
+    this.resetEditing();
+  }
+
+  resetEditing() {
+    this.editingField = null;
+    this.originalValue = null;
+    this.updatedValue = null;
+  }
+
+  handleKeyUp(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      this.cancelEditing();
+    } else if (event.key === 'Enter') {
+      this.saveField();
+    }
+  }
+
+  // ----------------------------------------------------------------------------
 }
